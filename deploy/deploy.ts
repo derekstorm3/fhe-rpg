0f7aaaa2e92340b75ebb6e2c3cee5a5ae728b0f7
derekstorm3 <wbfzxjo8855@hotmail.com>
2025-10-21 21:46:13 +0800

    finish deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,17 +1,25 @@
+import * as dotenv from "dotenv";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+dotenv.config();
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedQuiz = await deploy("EncryptedGameQuiz", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const infuraKey = process.env.INFURA_API_KEY;
+  if (!infuraKey) {
+    console.warn("INFURA_API_KEY is not set. Sepolia deployment will fail without it.");
+  }
+
+  console.log(`EncryptedQuiz contract: ${deployedQuiz.address}`);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_encryptedQuiz"; // id required to prevent reexecution
+func.tags = ["EncryptedQuiz"];
